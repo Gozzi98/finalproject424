daafc797114f6872cfe61f56aa9765df50fd4f65
David Gozlan <davidgozlan@Davids-MacBook-Air.local>
2023-11-27 14:38:10 -0500

    done

--- Project-COMP424-2023-Fall-main/agents/student_agent.py
+++ Project-COMP424-2023-Fall-main/agents/student_agent.py
@@ -6,7 +6,7 @@ import numpy as np
 from copy import deepcopy
 import time
 import logging
-
+import random
 # python simulator.py --player_1 student_agent --player_2 random_agent --display
 
 @register_agent("student_agent")
@@ -15,7 +15,7 @@ class StudentAgent(Agent):
     A dummy class for your implementation. Feel free to use this class to
     add any helper functionalities needed for your agent.
     """
-
+    START_TIME = None
     def __init__(self):
         super(StudentAgent, self).__init__()
         self.name = "StudentAgent"
@@ -45,12 +45,16 @@ class StudentAgent(Agent):
         # Some simple code to help you with timing. Consider checking 
         # time_taken during your search and breaking with the best answer
         # so far when it nears 2 seconds.
-        start_time = time.time()
+        global START_TIME
+        START_TIME = time.time()
         
-        
-        my_step = self.alpha_beta(chess_board, my_pos, adv_pos, max_step, -sys.maxsize, sys.maxsize, 0, 3)
+        if max_step >= 6 :
+            my_step = self.alpha_beta(chess_board, my_pos, adv_pos, max_step, -sys.maxsize, sys.maxsize, 0, 1)
+        else:
+            my_step = self.alpha_beta(chess_board, my_pos, adv_pos, max_step, -sys.maxsize, sys.maxsize, 0, 3)
 
-        time_taken = time.time() - start_time
+        time_taken = time.time() - START_TIME
+        print("max step is ", max_step)
         print("My AI's turn took ", time_taken, "seconds.")
 
         return my_step[0]
@@ -125,10 +129,11 @@ class StudentAgent(Agent):
         else:
             return self.p1, self.p1_pos, self.p0_pos
 
-    def heuristic(self,chess_board,my_pos,adv_pos,max_step):
+    def heuristic(self,chess_board,my_pos,adv_pos,max_step,depth):
         """
         Implement the heuristic function of your agent here.      
         """
+        global START_TIME
         cur_move_score = 0
         # function to calculate number of steps away from adv_pos
         endgame = self.check_endgame(chess_board, my_pos, adv_pos)
@@ -142,26 +147,33 @@ class StudentAgent(Agent):
             else:
                 # print(-10000)
                 return -20000
+        elif time.time() - START_TIME > 1.4:
+            return cur_move_score
         else:
             # calculate advs possible moves
-            moves = self.get_player_moves(chess_board, adv_pos, my_pos, max_step)
+            moves = self.get_player_moves(chess_board, adv_pos, my_pos, max_step,depth)
             cur_move_score -= len(moves)
             # check if my_pos is adjacent to two walls
             if self.check_two_walls(chess_board,my_pos):
                 cur_move_score -= 1000
-            if self.check_one_wall(chess_board,my_pos):
-                cur_move_score -= 750
+            # check if my_pos is adjacent to three walls
+            if self.check_three_wall(chess_board,my_pos):
+                cur_move_score -= 5000
         # print(cur_move_score)
         return cur_move_score
-    def check_one_wall(self,chess_board,my_pos):
+    def check_three_wall(self,chess_board,my_pos):
         """
-        Check if my_pos is adjacent to one wall
+        Check if my_pos is adjacent to three walls
         """
         r,c = my_pos
         for d in range(0,4):
-            if chess_board[r,c,d]:
-                return True
+            for d2 in range(0,4):
+                for d3 in range(0,4):
+                    if d != d2 and d != d3 and d2 != d3:
+                        if chess_board[r,c,d] and chess_board[r,c,d2] and chess_board[r,c,d3]:
+                            return True
         return False
+        
     def check_two_walls(self,chess_board,my_pos):
         """
         Check if my_pos is adjacent to two walls
@@ -174,15 +186,18 @@ class StudentAgent(Agent):
                         return True
         return False
    
-    def get_player_moves(self, chess_board, my_pos, adv_pos, max_step):
+    def get_player_moves(self, chess_board, my_pos, adv_pos, max_step,depth):
         moves = []
         dirs = ((-1, 0), (0, 1), (1, 0), (0, -1))
 
         for s in range(max_step):
-            self.take_steps(chess_board, my_pos, adv_pos, s, dirs, moves)
+            if depth == 0:
+                self.take_steps_old(chess_board, my_pos, adv_pos, s, dirs, moves)
+            else:
+                self.take_steps(chess_board, my_pos, adv_pos, s, dirs, moves)
         return moves
     
-    def take_steps(self,chess_board, my_pos, adv_pos, s, dirs, moves):
+    def take_steps_old(self,chess_board, my_pos, adv_pos, s, dirs, moves):
         # Get all possible moves for the current player
 
         r,c = my_pos
@@ -208,62 +223,93 @@ class StudentAgent(Agent):
                         moves.append((my_pos, d))
             return moves
 
-    
+    def take_steps(self,chess_board, my_pos, adv_pos, s, dirs, moves):
+        # Get all possible moves for the current player
+
+        r,c = my_pos
+        if s != 0:
+            for d in range(0,4):
+                t = random.randint(0,7)
+                if t<=4:
+                    # check if the move is valid
+                    my_pos = (r+dirs[d][0], c+dirs[d][1])
+                    if not chess_board[r, c, d] and not adv_pos == my_pos:
+                        self.take_steps(chess_board, my_pos, adv_pos, s-1, dirs, moves)
+        else:
+            my_pos_present = False
+            for pos in moves:
+                if pos is None:
+                    moves.remove(pos)
+                    continue
+                if np.array_equal(my_pos, pos[0]):
+                    my_pos_present = True
+                    break
+            if not my_pos_present:
+                for d in range(0,4):
+                    # check where can i place a wall
+                    if not chess_board[r, c, d]:
+                        moves.append((my_pos, d))
+            return moves
     
     def alpha_beta(self, chess_board, my_pos, adv_pos, max_step, alpha, beta, depth, max_depth):
         """
         Implement the alpha-beta pruning algorithm here.
         """
+        global START_TIME
+        dirs = ((-1, 0), (0, 1), (1, 0), (0, -1))
+         # get all possible moves for the current player
+        moves = self.get_player_moves(chess_board, my_pos, adv_pos, max_step,depth)
         # check if the game is over
-        if depth == max_depth or self.check_endgame(chess_board, my_pos, adv_pos)[0]:
+        if depth == max_depth or self.check_endgame(chess_board, my_pos, adv_pos)[0] or len(moves) == 0  or time.time() - START_TIME > 1:
 
-            return my_pos, self.heuristic(chess_board, my_pos, adv_pos, max_step)
-        # get all possible moves for the current player
-        moves = self.get_player_moves(chess_board, my_pos, adv_pos, max_step)
+            return my_pos, self.heuristic(chess_board, my_pos, adv_pos, max_step,depth)
+       
         best_move = moves[0]
         last_move = None
         # if it is my turn
         if depth % 2 == 0:
             max_eval = -sys.maxsize
             for move in moves:
+                if max_step >=6 and last_move == move[0]:
+                    continue
+                last_move = move[0] 
                 new_board = deepcopy(chess_board)
+                
                 # place the wall
                 new_board[move[0][0], move[0][1], move[1]] = True
+                new_board[move[0][0] + dirs[move[1]][0], move[0][1] + dirs[move[1]][1], (move[1] + 2) % 4] = True
 
-                if move[0] == last_move:
-                    continue
-                else:
-                    last_move = move[0]
-                    # get the next state
-                    next_state = self.alpha_beta(new_board, move[0], adv_pos, max_step, alpha, beta, depth+1, max_depth)
-                    # check if the next state is better than the current state
-                    if next_state[1] > max_eval:
-                        max_eval = next_state[1]
-                        best_move = move
-                    # check if the next state is better than the current state
-                    alpha = max(alpha, next_state[1])
-                    if beta <= alpha:
-                        break
+                # get the next state
+                next_state = self.alpha_beta(new_board, move[0], adv_pos, max_step, alpha, beta, depth+1, max_depth)
+                # check if the next state is better than the current state
+                if next_state[1] > max_eval :
+                    max_eval = next_state[1]
+                    best_move = move
+                # check if the next state is better than the current state
+                alpha = max(alpha, next_state[1])
+                if beta <= alpha:
+                    break
             return best_move, max_eval
         # if it is the adversary's turn
         else:
             min_eval = sys.maxsize
             for move in moves:
+                if max_step >=6 and last_move == move[0]:
+                    continue
+                last_move = move[0]
                 new_board = deepcopy(chess_board)
                 # place the wall
                 new_board[move[0][0], move[0][1], move[1]] = True
-                if move[0] == last_move:
-                    continue
-                else:
-                    last_move = move[0]
-                    # get the next state
-                    next_state = self.alpha_beta(new_board, my_pos, move[0], max_step, alpha, beta, depth+1, max_depth)
-                    # check if the next state is better than the current state
-                    if next_state[1] < min_eval:
-                        min_eval = next_state[1]
-                        best_move = move
-                    # check if the next state is better than the current state
-                    beta = min(beta, next_state[1])
-                    if beta <= alpha:
-                        break
+                new_board[move[0][0] + dirs[move[1]][0], move[0][1] + dirs[move[1]][1], (move[1] + 2) % 4] = True
+                
+                # get the next state
+                next_state = self.alpha_beta(new_board, my_pos, move[0], max_step, alpha, beta, depth+1, max_depth)
+                # check if the next state is better than the current state
+                if next_state[1] < min_eval:
+                    min_eval = next_state[1]
+                    best_move = move
+                # check if the next state is better than the current state
+                beta = min(beta, next_state[1])
+                if beta <= alpha:
+                    break
             return best_move,beta
